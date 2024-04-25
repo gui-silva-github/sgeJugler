8fef990deefbf8bcdb812cb5c00a81a52fb71f2c
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-04-24 22:27:36 -0300

    Edições no Banco de Dados

---
+++ editar/edita_curso.php
@@ -0,0 +1,17 @@
+<?php
+
+    include '../db.php';
+
+    $id_curso = $_POST['id_curso'];
+    $nome_curso = $_POST['nome_curso'];
+    $carga_horaria = $_POST['carga_horaria'];
+
+    $query = "UPDATE cursos SET nome_curso='$nome_curso', carga_horaria=$carga_horaria WHERE id_curso = $id_curso";
+
+    $stm = $con->prepare($query);
+
+    $stm->execute();
+
+    header('location:../index.php?pagina=cursos');
+
+?>
